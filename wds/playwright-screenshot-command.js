4931e3af9787bb1c68e33498fb6cb8ce6966714a
Florian Sanders <florian.sanders@clever-cloud.com>
2025-06-19 08:29:45 +0200

    --wip-- [skip ci]

--- wds/playwright-screenshot-command.js
+++ wds/playwright-screenshot-command.js
@@ -1,16 +1,22 @@
+import { expect, test } from '@playwright/test';
+
 const playwrightScreenshot = async ({ provider, page }) => {
   if (provider.name === 'playwright') {
-    const screenshot = await page.screenshot({ animations: 'disabled' });
+    test('toto', async () => {
+      await expect(page).toHaveScreenshot();
+
+      console.log('screenshotting');
+
+      return { someValue: true };
+    });
     // Save the screenshot to the screenshots directory (create if it doesn't exist)
-    console.log('screenshotting');
-    const fs = await import('fs/promises');
-    const path = await import('path');
-    const screenshotsDir = path.resolve(process.cwd(), 'screenshots');
-    await fs.mkdir(screenshotsDir, { recursive: true });
-    const filename = `screenshot-${Date.now()}.png`;
-    const filepath = path.join(screenshotsDir, filename);
-    await fs.writeFile(filepath, screenshot);
-    return { someValue: true };
+    // const fs = await import('fs/promises');
+    // const path = await import('path');
+    // const screenshotsDir = path.resolve(process.cwd(), 'screenshots');
+    // await fs.mkdir(screenshotsDir, { recursive: true });
+    // const filename = `screenshot-${Date.now()}.png`;
+    // const filepath = path.join(screenshotsDir, filename);
+    // await fs.writeFile(filepath, screenshot);
   }
 
   throw new Error(`provider ${provider.name} is not supported`);
